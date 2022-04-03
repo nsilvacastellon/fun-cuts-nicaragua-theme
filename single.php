a2012a5d628ca52b838ec94dd7a3541b9c5cd836
Jose Silva <jnscsilvacastellon@gmail.com>
2022-04-03 00:13:32 -0700

    Fixed li elements margins on single.php content area

--- single.php
+++ single.php
@@ -9,7 +9,9 @@ while(have_posts()) {
     the_post(); ?>
     <h2><?php the_title(); ?></h2>
     <p class="blog-author-info">Publicado por: <?php the_author_posts_link(); ?> Fecha: <?php the_time('n-j-Y'); ?> Categoria(s): <?php echo get_the_category_list(', '); ?></p>
-    <?php the_content(); ?>
+    <div class="single-blog-content">
+        <?php the_content(); ?>
+    </div>
     
 <?php } ?>
 
