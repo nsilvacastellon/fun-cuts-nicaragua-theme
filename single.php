58d6b18d22a93840760c378f81e96cab09513c05
Jose Silva <jnscsilvacastellon@gmail.com>
2022-04-03 23:59:55 -0700

    Add author info styles to single.php

--- single.php
+++ single.php
@@ -8,6 +8,7 @@ while(have_posts()) {
 
     the_post(); ?>
     <h2><?php the_title(); ?></h2>
+    <p class="blog-author-info">Publicado por: <?php the_author_posts_link(); ?> Fecha: <?php the_time('n-j-Y'); ?> Categoria(s): <?php echo get_the_category_list(', '); ?></p>
     <?php the_content(); ?>
     
 <?php } ?>
