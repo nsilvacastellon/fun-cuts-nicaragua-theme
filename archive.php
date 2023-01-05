5dff087afc6d83da34d81b8fdfdbe89791f585b6
Jose Silva <jnscsilvacastellon@gmail.com>
2023-01-04 23:59:16 -0800

    Set post thumbnail 2 dif sizes 150x150 and 300x300

--- archive.php
+++ archive.php
@@ -22,7 +22,7 @@ get_header();
               <div class="flex-child-blog-item">
                 <!-- <article class="blog-item"> -->
                     
-                    <div class="feature-img"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a></div>
+                    <div class="feature-img"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('index-post-thumbnail'); ?></a></div>
                     <h2 class="blog-item-h2"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
                     <p class="blog-author-info">Publicado por: <?php the_author_posts_link(); ?> Fecha: <?php the_time('n-j-Y'); ?> Categoria(s): <?php echo get_the_category_list(', '); ?></p>
                     <p class="excerpt"><?php echo wp_trim_words(get_the_content(), 15); ?></p>
