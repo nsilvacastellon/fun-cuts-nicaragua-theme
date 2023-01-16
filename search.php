4dcdcf414b8fbdc3f38bb00462578cc09023d132
Jose Silva <jnscsilvacastellon@gmail.com>
2023-01-15 23:52:06 -0800

    Fixed the layout issue with get_template_part function. Added content-page.php and content-post.php

--- search.php
+++ search.php
@@ -13,19 +13,11 @@ get_header();
           <?php
           
           while(have_posts()) {
-            the_post(); ?>
+            the_post(); 
+            get_template_part('template-parts/content', get_post_type());
+            ?>
             
-              <div class="flex-child-blog-item">
-                <!-- <article class="blog-item"> -->
-                    
-                    <div class="feature-img"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('index-post-thumbnail'); ?></a></div>
-                    <h2 class="blog-item-h2"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-                    <p class="blog-author-info">Publicado por: <?php the_author_posts_link(); ?> Fecha: <?php the_time('n-j-Y'); ?> Categoria(s): <?php echo get_the_category_list(', '); ?></p>
-                    <p class="excerpt"><?php echo wp_trim_words(get_the_content(), 15); ?></p>
-                    <p class="read-more"><a class="cta" href="<?php the_permalink(); ?>">Leer mas &raquo;</a></p>
-                <!-- </article> -->
-
-              </div> <!-- flex-child-blog-item ends here -->
+              
             
           <?php } ?>
 
